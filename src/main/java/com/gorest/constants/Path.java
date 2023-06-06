535ebc8fd87bbe8431e6489ac65b42c01104ced6
Krp8592 <patelkashyap8555@gmail.com>
2023-06-06 12:39:50 +0100

    Week-18-Hw

---
+++ src/main/java/com/gorest/constants/Path.java
@@ -0,0 +1,13 @@
+package com.gorest.constants;
+
+/**
+ * Created By Kashyap patel
+ */
+public class Path {
+
+    /*
+     * This is base path for student api
+     */
+    public static final String USER = "/public/v2";
+
+}
